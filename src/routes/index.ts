82bfbb36eaaa42af9a1f64bc5b54bd9665dbedb6
Lucas Hideki <ls_hdk@protonmail.com>
2020-09-02 10:12:40 -0300

    check_email_password

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 // src/routes/index.ts
 import { Router } from 'express';
 import appointmentsRouter from './appointments.routes';
-import userRouter from './user.routes';
+import userRouter from './users.routes';
+import sessionsRouter from './sessions.routes';
 
 const routes = Router();
 routes.use('/appointments', appointmentsRouter); //  Toda rota que iniciar com /apointments será repasado para dentro do appointmentsRouter
 routes.use('/users', userRouter);
+routes.use('/sessions', sessionsRouter);
 export default routes;
