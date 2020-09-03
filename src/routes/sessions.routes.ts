77ecc78c83f8711d1cdfaf04c81bbd070ec77c15
Lucas Hideki <ls_hdk@protonmail.com>
2020-09-03 00:43:35 -0300

    error_handling

--- src/routes/sessions.routes.ts
+++ src/routes/sessions.routes.ts
@@ -5,18 +5,14 @@ const sessionsRouter = Router();
 
 //  POST http://localhost:3333/appointments
 sessionsRouter.post('/', async (request, response) => {
-  try {
-    const { email, password } = request.body;
-    const authenticateUser = new AuthenticateUserService();
+  const { email, password } = request.body;
+  const authenticateUser = new AuthenticateUserService();
 
-    const { user, token } = await authenticateUser.execute({ email, password });
+  const { user, token } = await authenticateUser.execute({ email, password });
 
-    delete user.password;
+  delete user.password;
 
-    return response.json({ user, token });
-  } catch (err) {
-    return response.status(err.statusCode).json({ error: err.message });
-  }
+  return response.json({ user, token });
 });
 
 export default sessionsRouter;
