9be66624db37c87b04256b2dba85de396a5497db
Lucas Hideki <ls_hdk@protonmail.com>
2020-11-25 16:39:48 -0300

    Liskov

---
+++ src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -0,0 +1,17 @@
+import { EntityRepository, Repository } from 'typeorm';
+import IappointmentRepository from '@modules/appointments/repositories/IAppointmentsRepository';
+import Appointment from '../entities/Appointment';
+
+@EntityRepository(Appointment)
+class AppointmentsRepository
+  extends Repository<Appointment>
+  implements IappointmentRepository {
+  public async findByDate(date: Date): Promise<Appointment | undefined> {
+    const findAppointment = await this.findOne({
+      where: { date },
+    });
+    return findAppointment;
+  }
+}
+
+export default AppointmentsRepository;
