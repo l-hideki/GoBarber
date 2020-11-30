2157785fc0511d7f90f21943cc4cae45d0e63612
Lucas Hideki <ls_hdk@protonmail.com>
2020-11-30 18:44:11 -0300

    first_test

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,20 @@
-test('sum two numbers', () => {
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const CreateAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+    const appointment = await CreateAppointment.execute({
+      date: new Date(),
+      provider_id: '123',
+    });
+    expect(appointment).toHaveProperty('id');
+  });
+
+  /*   it('should not be able to create two appointments on the same time', () => {
   expect(1 + 2).toBe(3);
+}); */
 });
