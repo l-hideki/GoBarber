6a8a5479892fcc4078e9fd1c2a5af58739b4473b
Lucas Hideki <ls_hdk@protonmail.com>
2020-12-03 18:09:33 -0300

    password_reset_test

--- src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
+++ src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
@@ -11,6 +11,13 @@ class FakeUserTokensRepository implements IUserTokensRepository {
     this.usersTokens.push(userToken);
     return userToken;
   }
+
+  public async findByToken(token: string): Promise<UserToken | undefined> {
+    const userToken = this.usersTokens.find(
+      findToken => findToken.token === token,
+    );
+    return userToken;
+  }
 }
 
 export default FakeUserTokensRepository;
---
+++ src/modules/users/services/ResetPasswordService.ts
@@ -0,0 +1,34 @@
+import AppError from '@shared/errors/AppErrors';
+import { inject, injectable } from 'tsyringe';
+import IUsersRepository from '../repositories/IUsersRepository';
+import IUserTokensRepository from '../repositories/IUserTokensRepository';
+
+interface IRequest {
+  token: string;
+  password: string;
+}
+@injectable()
+class ResetPasswordService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+
+    @inject('UserTokensRepository')
+    private userTokensRepository: IUserTokensRepository,
+  ) {}
+
+  public async execute({ password, token }: IRequest): Promise<void> {
+    const userToken = await this.userTokensRepository.findByToken(token);
+    if (!userToken) {
+      throw new AppError('user token does not exists');
+    }
+    const user = await this.usersRepository.findById(userToken.user_id);
+    if (!user) {
+      throw new AppError('user does not exists');
+    }
+    user.password = password;
+    await this.usersRepository.save(user);
+  }
+}
+
+export default ResetPasswordService;
