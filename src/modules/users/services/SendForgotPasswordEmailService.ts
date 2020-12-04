2343a4bcc68ce72284e4e7dda28f032b759fedfd
Lucas Hideki <ls_hdk@protonmail.com>
2020-12-04 17:42:18 -0300

    template_engine

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -1,4 +1,5 @@
 import { inject, injectable } from 'tsyringe';
+import path from 'path';
 import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
 import AppError from '@shared/errors/AppErrors';
 import IUsersRepository from '../repositories/IUsersRepository';
@@ -27,6 +28,12 @@ class SendForgotPasswordEmailService {
       throw new AppError('User does not exists.');
     }
     const { token } = await this.userTokensRepository.generate(user.id);
+    const forgotPasswordTemplate = path.resolve(
+      __dirname,
+      '..',
+      'views',
+      'forgot_password.hbs',
+    );
 
     await this.MailProvider.sendMail({
       to: {
@@ -35,10 +42,10 @@ class SendForgotPasswordEmailService {
       },
       subject: '[GoBarber] Recuperação de senha',
       templateData: {
-        template: 'Olá, {{name}}: {{token}}',
+        file: forgotPasswordTemplate,
         variables: {
           name: user.name,
-          token,
+          link: `http://localhost:3000/reset_password?token=${token}`,
         },
       },
     });
