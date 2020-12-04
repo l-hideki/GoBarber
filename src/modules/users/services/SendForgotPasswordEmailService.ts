c9c02b928eaef72ed4317b9fe1ed59509a6d2660
Lucas Hideki <ls_hdk@protonmail.com>
2020-12-04 15:37:50 -0300

    email_template

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -28,10 +28,20 @@ class SendForgotPasswordEmailService {
     }
     const { token } = await this.userTokensRepository.generate(user.id);
 
-    await this.MailProvider.sendMail(
-      email,
-      `Pedido de recuperação de senha recebido: ${token}`,
-    );
+    await this.MailProvider.sendMail({
+      to: {
+        name: user.name,
+        email: user.email,
+      },
+      subject: '[GoBarber] Recuperação de senha',
+      templateData: {
+        template: 'Olá, {{name}}: {{token}}',
+        variables: {
+          name: user.name,
+          token,
+        },
+      },
+    });
   }
 }
 
